25a2dbd6a1400423c0dae5b3ae2e87e9af9f611f
holtale1 <holtale1@msu.edu>
2021-11-17 16:00:54 -0500

    Added averaging for various IV values

--- catchcalc.py
+++ catchcalc.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 import math
 import pprint
+from typing import List, Tuple
 import requests
 
 class Pokeball(Enum):
@@ -37,13 +38,58 @@ class Status(Enum):
     BURN = 6
     
 
+@dataclass(frozen=True)
+class WobbleProbability:
+    zero_wobbles: float
+    one_wobble: float
+    two_wobbles: float
+    three_wobbles: float
+
 
 @dataclass(frozen=True)
 class CatchProbability:
+    hp_iv_range: Tuple[int, int]
     CC_chance: float
     CC_success: float
     catch_chance: float
     total_chance: float
+    wobble_probabilities: WobbleProbability
+    
+
+@dataclass(frozen=True)
+class CatchCalcResult:
+    catch_probabilities: List[CatchProbability]
+    
+    def average_probability(self) -> CatchProbability:
+        max_iv = 0
+        avg_CC_chance = avg_CC_success = 0.0
+        avg_catch_chance = avg_total_chance = 0.0
+        avg_wobble_probabilities = [0.0, 0.0, 0.0, 0.0]
+        for cp in self.catch_probabilities:
+            if cp.hp_iv_range[1] > max_iv:
+                max_iv = cp.hp_iv_range[1]
+            avg_CC_chance += cp.CC_chance
+            avg_CC_success += cp.CC_success
+            avg_catch_chance += cp.catch_chance
+            avg_total_chance += cp.total_chance
+            avg_wobble_probabilities[0] += cp.wobble_probabilities.zero_wobbles
+            avg_wobble_probabilities[1] += cp.wobble_probabilities.one_wobble
+            avg_wobble_probabilities[2] += cp.wobble_probabilities.two_wobbles
+            avg_wobble_probabilities[3] += cp.wobble_probabilities.three_wobbles
+            
+        return CatchProbability(
+            hp_iv_range=(0, max_iv),
+            CC_chance=round(avg_CC_chance/len(self.catch_probabilities), 5),
+            CC_success=round(avg_CC_success/len(self.catch_probabilities), 5),
+            catch_chance=round(avg_catch_chance/len(self.catch_probabilities), 5),
+            total_chance=round(avg_total_chance/len(self.catch_probabilities), 5),
+            wobble_probabilities=WobbleProbability(
+                zero_wobbles=round(avg_wobble_probabilities[0]/len(self.catch_probabilities), 5),
+                one_wobble=round(avg_wobble_probabilities[1]/len(self.catch_probabilities), 5),
+                two_wobbles=round(avg_wobble_probabilities[2]/len(self.catch_probabilities), 5),
+                three_wobbles=round(avg_wobble_probabilities[3]/len(self.catch_probabilities), 5)
+            )
+        )
 
 
 def catch_calc_gen8(
@@ -63,13 +109,13 @@ def catch_calc_gen8(
     night_or_cave: bool = False,
     opposite_gender: bool = False,
     exactly_one_hp: bool = False,
-) -> CatchProbability:
+) -> CatchCalcResult:
     
     def get_max_hp(base_hp: int, level: int, iv: int) -> int:
         if base_hp == 1:
             return 1  # shedinja always has 1 hp
         else:
-            return math.floor(((2 * base_hp * iv) * level) / 100) + level + 10
+            return math.floor(((2 * base_hp + iv) * level) / 100) + level + 10
     
     # Formula:
     # X = ( [ ( 3M - 2H ) * G * C * B ] / 3M ) * L * S * D
@@ -236,26 +282,7 @@ def catch_calc_gen8(
     if not completed_main_story:
         if your_pokemon_level < pokemon_level:
             D = 410/4096
-            
-    #
-    # Calc Final Capture Rate
-    #
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}/")
-    base_hp = 0
-    if response.status_code == 200:
-        p_stats = response.json()["stats"]
-        for p_stat in p_stats:
-            if p_stat["stat"]["name"] == "hp":
-               base_hp = int(p_stat["base_stat"])
-               break
-    else:
-        print(f"Error: Failed to obtain data for '{pokemon_name}'. Status code = {response.status_code}")
-        
-    M = get_max_hp(base_hp=base_hp, level=pokemon_level, iv=0)
-    H = 1 if exactly_one_hp or M == 1 else math.floor((pokemon_hp_percent / 100) * M)
-        
-    X = (((3*M - 2*H) * G * C * B) / (3 * M)) * L * S * D
-     
+
     #
     # Calc Critical Capture vars
     #
@@ -274,26 +301,108 @@ def catch_calc_gen8(
         P = 0.5
     else:
         P = 0
-            
+
     #
-    # Calc Critical Capture chance
+    # Calc Final Capture Rates
     #
-    CC = math.floor( ( min(255,X) * P * Ch ) / 6 )
-    CC_chance = CC / 256
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}/")
+    base_hp = 0
+    if response.status_code == 200:
+        p_stats = response.json()["stats"]
+        for p_stat in p_stats:
+            if p_stat["stat"]["name"] == "hp":
+               base_hp = int(p_stat["base_stat"])
+               break
+    else:
+        print(f"Error: Failed to obtain data for '{pokemon_name}'. Status code = {response.status_code}")
     
     #
-    # Calc Overall Capture chance
+    # Calc for all possible IVs
     #
-    overall_chance = (CC_chance * math.pow(X / 255, 0.1875)) + ((1-CC_chance) * math.pow(X / 255, 0.75))
+    min_iv = 0
+    current_CC_chance = current_CC_success = 0.0
+    current_catch_chance = current_total_chance = 0.0
+    current_wobble_probabilities = [0.0, 0.0, 0.0, 0.0]
+    cp_list = []
+    for iv in range(32):
+        M = get_max_hp(base_hp=base_hp, level=pokemon_level, iv=iv)
+        H = 1 if exactly_one_hp or M == 1 else math.floor((pokemon_hp_percent / 100) * M)
+        
+        X = (((3*M - 2*H) * G * C * B) / (3 * M)) * L * S * D
+        
+        #
+        # Calc Critical Capture chance
+        #
+        CC = math.floor( ( min(255,X) * P * Ch ) / 6 )
+        CC_chance = round(CC / 256, 5)
+        CC_success = round(math.pow(X / 255, 0.1875), 5)
+        
+        #
+        # Calc Break out attempts
+        #
+        Y = math.floor(65536 / (math.pow(255 / X, 3 / 16)))
+        temp_wobble_probs = [0.0, 0.0, 0.0, 0.0]
+        for wobble in range(4):
+            temp_wobble_probs[wobble] = round((1 - CC_chance) * (1 - (Y / 65536)) * math.pow(Y / 65536, wobble), 5)
+        
+        #
+        # Calc Overall Capture chance
+        #
+        catch_chance = round(math.pow(X / 255, 0.75), 5)
+        overall_chance = round((CC_chance * CC_success) + ((1-CC_chance) * catch_chance), 5)
+        
+        if iv == 0:  # first run
+            current_CC_chance = CC_chance
+            current_CC_success = CC_success
+            current_catch_chance = catch_chance
+            current_total_chance = overall_chance
+            current_wobble_probabilities = temp_wobble_probs[:]
+        else:
+            if current_CC_chance != CC_chance or \
+               current_CC_success != CC_success or \
+               current_catch_chance != catch_chance or \
+               current_total_chance != overall_chance or \
+               current_wobble_probabilities != temp_wobble_probs:  # different result
+                cp_list.append(CatchProbability(
+                    hp_iv_range=(min_iv, iv-1),
+                    CC_chance=current_CC_chance,
+                    CC_success=current_CC_success,
+                    catch_chance=current_catch_chance,
+                    total_chance=current_total_chance,
+                    wobble_probabilities=WobbleProbability(
+                        zero_wobbles=current_wobble_probabilities[0],
+                        one_wobble=current_wobble_probabilities[1],
+                        two_wobbles=current_wobble_probabilities[2],
+                        three_wobbles=current_wobble_probabilities[3]
+                    )
+                ))
+                min_iv = iv
+                current_CC_chance = CC_chance
+                current_CC_success = CC_success
+                current_catch_chance = catch_chance
+                current_total_chance = overall_chance
+                current_wobble_probabilities = temp_wobble_probs[:]
+            if iv == 31:
+                cp_list.append(CatchProbability(
+                    hp_iv_range=(min_iv, iv),
+                    CC_chance=current_CC_chance,
+                    CC_success=current_CC_success,
+                    catch_chance=current_catch_chance,
+                    total_chance=current_total_chance,
+                    wobble_probabilities=WobbleProbability(
+                        zero_wobbles=current_wobble_probabilities[0],
+                        one_wobble=current_wobble_probabilities[1],
+                        two_wobbles=current_wobble_probabilities[2],
+                        three_wobbles=current_wobble_probabilities[3]
+                    )
+                ))
     
-    return CatchProbability(
-        CC_chance=CC_chance,
-        CC_success=math.pow(X / 255, 0.1875),
-        catch_chance=math.pow(X / 255, 0.75),
-        total_chance=overall_chance
+    return CatchCalcResult(
+        catch_probabilities=cp_list
     )
-    
-result = catch_calc_gen8(
+
+
+result_obj = catch_calc_gen8(
     pokemon_name="beldum",
     pokemon_level=68,
     pokemon_hp_percent=1,
@@ -307,8 +416,14 @@ result = catch_calc_gen8(
     exactly_one_hp=True
 )
 
+result = result_obj.average_probability()
+
 print(f"CC={result.CC_chance}")
 print(f"CC_success={result.CC_success}")
 print(f"Catch={result.catch_chance}")
 print(f"Total={result.total_chance}")
+print(f"0:{result.wobble_probabilities.zero_wobbles}, 1:{result.wobble_probabilities.one_wobble}, 2:{result.wobble_probabilities.two_wobbles}, 3:{result.wobble_probabilities.three_wobbles}")
 
+print(len(result_obj.catch_probabilities))
+for cp in result_obj.catch_probabilities:
+    print(f"IV: {cp.hp_iv_range[0]}-{cp.hp_iv_range[1]}")
